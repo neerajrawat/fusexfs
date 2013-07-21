3d2fb1581303185dead41042b63b91d48285ab16
neerajrawat <neerajrawat2011@gmail.com>
2013-07-22 00:50:33 +0530

    modification in include lines

--- src/fuse/fuse_xfs.c
+++ src/fuse/fuse_xfs.c
@@ -16,8 +16,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include "fuse.h"
-#include "fuse_lowlevel.h"
+#include <fuse.h>
+#include <fuse_lowlevel.h>
 /*************************/
 
 #include <string.h>
@@ -230,7 +230,7 @@ fuse_xfs_statfs(const char *path, struct statvfs *stbuf) {
     stbuf->f_ffree = mount->m_sb.sb_ifree + mount->m_maxicount - mount->m_sb.sb_icount;
     stbuf->f_favail = stbuf->f_ffree;
     stbuf->f_namemax = MAXNAMELEN;
-    stbuf->f_fsid = *((unsigned long*)mount->m_sb.sb_uuid);
+    /*stbuf->f_fsid = *((unsigned long*)mount->m_sb.sb_uuid);*/
     log_debug("f_bsize=%ld\n", stbuf->f_bsize);
     log_debug("f_frsize=%ld\n", stbuf->f_frsize);
     log_debug("f_blocks=%d\n", stbuf->f_blocks);
